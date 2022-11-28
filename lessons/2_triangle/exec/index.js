31091c4e5c411f5c26db51c50b8c19f371c7491c
yyc-git <395976266@qq.com>
2022-11-28 13:46:02 +0800

    feat(2_triangle): add project

---
+++ lessons/2_triangle/exec/index.js
@@ -0,0 +1,11 @@
+let _main = async () => {
+    //TODO init
+
+    while (true) {
+        // TODO render
+    }
+};
+
+_main().then(() => {
+    console.log("finish main");
+});
---
+++ lessons/2_triangle/exec/scene.vert
@@ -0,0 +1,8 @@
+#version 450
+#pragma shader_stage(vertex)
+
+layout(location = 0) in vec2 position;
+
+void main() {
+    // TODO implement
+}
