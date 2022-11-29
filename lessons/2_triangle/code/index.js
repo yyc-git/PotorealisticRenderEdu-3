12272c43e64b890be87830c6f8b3bb7ad5879fa5
yyc-git <395976266@qq.com>
2022-11-30 06:13:32 +0800

    feat(2_triangle): update code

--- lessons/2_triangle/code/index.js
+++ lessons/2_triangle/code/index.js
@@ -4,29 +4,33 @@ import path from "path";
 
 let _render = ([device, window, swapChain, queue], [vertexBuffer, indexBuffer, triangleIndices, pipeline]) => {
     let backBufferView = swapChain.getCurrentTextureView();
+    // 操作buffer，通过Encoder
     let commandEncoder = device.createCommandEncoder({});
     let renderPass = commandEncoder.beginRenderPass({
         colorAttachments: [{
-            clearColor: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
-            loadOp: "clear",
-            storeOp: "store",
-            attachment: backBufferView
+            clearColor: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },//背景色
+            loadOp: "clear",// 渲染这一帧前，存储或者删除
+            storeOp: "store",// 渲染完这一帧后，存储或者删除
+            attachment: backBufferView // view
         }]
     });
-    renderPass.setPipeline(pipeline);
+    renderPass.setPipeline(pipeline); // 用哪个管线
+    // 设置顶点和索引buffer
     renderPass.setVertexBuffer(0, vertexBuffer, 0);
     renderPass.setIndexBuffer(indexBuffer);
+    // 绘制3次  triangleIndices.length三角形顶点数   1是绘制的实例数量
     renderPass.drawIndexed(triangleIndices.length, 1, 0, 0, 0);
+    // 结束绘制
     renderPass.endPass();
-
+    // 提交绘制结果
     let commandBuffer = commandEncoder.finish();
     queue.submit([commandBuffer]);
-
     swapChain.present();
     window.pollEvents();
 };
 
 let _main = async () => {
+    // webgpu 对象初始化
     let window = new WebGPU.WebGPUWindow({
         width: 640,
         height: 480,
@@ -48,18 +52,20 @@ let _main = async () => {
         device: device,
         format: swapChainFormat
     });
-
+    // 顶点，索引
     let triangleVertices = new Float32Array([
         0.0, 0.5, 
         -0.5, -0.5,
         0.5, -0.5
     ]);
+    // 申请内存
     let vertexBuffer = device.createBuffer({
         size: triangleVertices.byteLength,
         usage: WebGPU.GPUBufferUsage.VERTEX | WebGPU.GPUBufferUsage.COPY_DST
     });
+    // 数据写入buffer
     vertexBuffer.setSubData(0, triangleVertices);
-
+    // 顶点索引
     let triangleIndices = new Uint32Array([
         0, 1, 2
     ]);
@@ -67,15 +73,16 @@ let _main = async () => {
         size: triangleIndices.byteLength,
         usage: WebGPU.GPUBufferUsage.INDEX | WebGPU.GPUBufferUsage.COPY_DST
     });
+    // 索引写入
     indexBuffer.setSubData(0, triangleIndices);
-
+    // layout定义数据的布局关系
     let layout = device.createPipelineLayout({
         bindGroupLayouts: []
     });
-
+    // 创建顶点shader和片元shader
     let vertexShaderModule = device.createShaderModule({ code: fs.readFileSync(path.join(process.cwd(), "lessons/2_triangle/code/scene.vert"), "utf-8") });
     let fragmentShaderModule = device.createShaderModule({ code: fs.readFileSync(path.join(process.cwd(), "lessons/2_triangle/code/scene.frag"), "utf-8") });
-
+    // 创建渲染管线
     let pipeline = device.createRenderPipeline({
         layout,
         vertexStage: {
@@ -86,15 +93,16 @@ let _main = async () => {
             module: fragmentShaderModule,
             entryPoint: "main"
         },
-        primitiveTopology: "triangle-list",
+        primitiveTopology: "triangle-list", // 三角面绘制
         vertexState: {
-            indexFormat: "uint32",
+            indexFormat: "uint32", 
+            // 
             vertexBuffers: [{
                 arrayStride: 2 * Float32Array.BYTES_PER_ELEMENT,
                 stepMode: "vertex",
                 attributes: [{
                     shaderLocation: 0,
-                    offset: 0,
+                    offset: 0, // 数据移位
                     format: "float2"
                 }]
             }]
@@ -107,6 +115,7 @@ let _main = async () => {
     });
 
     while (true) {
+        // 渲染
         _render([device, window, swapChain, queue], [vertexBuffer, indexBuffer, triangleIndices, pipeline]);
     }
 };
